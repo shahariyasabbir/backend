251090d2bfbde20d32f236753e56c98b0d998ef7
personal_website_sabbir_16 <ahanafsabbir83@gmail.com>
2025-12-28 18:51:14 +0600

    firsts comment

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.use(function(req,res,next){
 console.log("middle");
@@ -12,8 +14,11 @@ next();
 }); 
 
 
-app.get("/", function(req,res){
+app.get("/about", function(req,res){
     res.send("champion is rahi");
 });// its the routes
+app.get("/profile",function(req,res,next){
+    return next(new Error("Not implemented"));
+});
 
 app.listen(3000);
